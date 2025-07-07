2caad9649ba9bd6206d72428db5ca07dcaaf337b
David Doan <vandoandinh@gmail.com>
2025-07-07 10:02:16 -0700

    Add async to anonymous function

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -3,6 +3,6 @@ import { Router } from 'express';
 
 const userRouter = Router();
 
-userRouter.get('/dashboard', (req, res) => {});
+userRouter.get('/dashboard', async (req, res) => {});
 
 export { userRouter };
